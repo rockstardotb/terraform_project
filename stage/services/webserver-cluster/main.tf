37d0196634775ffe827921c647e05f4e98212d6c
rockstardotb <alex37184@yahoo.com>
2018-09-12 14:35:50 -0500

    updated

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -2,25 +2,6 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-variable "server_port" {
-	description = "The port the server will use for HTTP requests"
-	default = 8080
-}
-
-variable "elb_port_i" {
-        description = "The port the elb will use for HTTP requests"
-        default = 80
-}
-
-variable "elb_port_e" {
-        description = "The port the elb will use for health checks"
-        default = 0
-}
-
-output "elb_dns_name" {
-	value = "${aws_elb.example.dns_name}"
-}
-
 data "aws_availability_zones" "all" {}
 
 resource "aws_security_group" "instance" {
